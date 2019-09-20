bb16a452aa567a631c79ca921e0afe5ecbe22b5c
DanCRichards <dan.c.richards@gmail.com>
2019-09-20 22:26:11 +1200

    Added Planet API

--- SolarSystemPrototype/Assets/Recourses/Scripts/APIManager.cs
+++ SolarSystemPrototype/Assets/Recourses/Scripts/APIManager.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 using System.Net;
 using System;
 using System.IO;
+using Newtonsoft.Json;
+using APIObjects;
 
 public class APIManager : MonoBehaviour
 {
    
    void GetPlanets()
         {
-          HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("https://api.le-systeme-solaire.net/rest/bodies/"));
+          HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("https://api.le-systeme-solaire.net/rest/bodies/?filter[]=isPlanet,neq,false"));
           HttpWebResponse response = (HttpWebResponse)request.GetResponse();
           StreamReader reader = new StreamReader(response.GetResponseStream());
           string jsonResponse = reader.ReadToEnd();
           Debug.Log(jsonResponse);
-      
+          var bodies = Bodies.FromJson(jsonResponse);
+
+        Debug.Log(bodies.BodiesBodies[0].Name);
+
+        foreach(Body b in bodies.BodiesBodies)
+        {
+
         }
+
+    }
     // Start is called before the first frame update
     void Start()
     {
