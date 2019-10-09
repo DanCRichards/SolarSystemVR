9d3e95ad053660bbcad98241bce87b8bf021c1e3
Daniel Richards <dric372@UoA.auckland.ac.nz>
2019-10-09 15:58:02 +1300

    Added Card Animation, Falling Hammer + moving hammer

---
+++ SolarSystemPrototype/Assets/Resources/Scripts/PlanetAPIManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System;
+using System.IO;
+using APIObjects;
+using TMPro;
+
+namespace ApiManager
+{
+
+    public class APIManager : MonoBehaviour
+    {
+
+        public static Bodies bodies;
+
+        void GetPlanets()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("https://api.le-systeme-solaire.net/rest/bodies/?filter[]=isPlanet,neq,false"));
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string jsonResponse = reader.ReadToEnd();
+            Debug.Log(jsonResponse);
+            bodies = Bodies.FromJson(jsonResponse);
+
+            Debug.Log(bodies.BodiesBodies[0].Name);
+
+
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            GetPlanets();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
