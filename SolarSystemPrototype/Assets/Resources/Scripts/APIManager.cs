f34d3e2612d0d13cef8b84db76c05215fc4933d5
Dan Richards <dan.c.richards@gmail.com>
2019-10-10 12:00:24 +1300

    Small stuff

---
+++ SolarSystemPrototype/Assets/Resources/Scripts/APIManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System;
+using System.IO;
+using APIObjects;
+using TMPro;
+
+namespace ApiManager
+{
+
+    public class APIManager : MonoBehaviour
+    {
+
+        public static Bodies bodies;
+
+        void GetPlanets()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("https://api.le-systeme-solaire.net/rest/bodies/?filter[]=isPlanet,neq,false"));
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string jsonResponse = reader.ReadToEnd();
+            Debug.Log(jsonResponse);
+            bodies = Bodies.FromJson(jsonResponse);
+
+            Debug.Log(bodies.BodiesBodies[0].Name);
+
+
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            GetPlanets();
+        }
+
+
+        public static Body getPlanetInfo(string planet)
+        {
+            // Should look into actually returning object! 
+
+            foreach(Body bd in bodies.BodiesBodies)
+            {
+                if (bd.EnglishName == planet)
+                {
+                    return bd;
+                }
+            }
+            return new Body(); // Not good programming. Should actually do something else. 
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ SolarSystemPrototype/Assets/Resources/Scripts/getPlanetInfo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using ApiManager;
+using APIObjects;
+
+public class getPlanetInfo : MonoBehaviour
+{
+    public string planet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Body bd = APIManager.getPlanetInfo("Earth");
+        // Earth information here. 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
